07fc01c2c77502e9634ff9a9b40f8c25555f0223
Ivan Bondar <ivan.bondar@elogic.com.ua>
2020-08-08 17:24:25 +0300

    update

---
+++ start.php
@@ -0,0 +1,87 @@
+<HTML>
+<HEAD>
+  <title>Український хостинг - початкова сторінка</title>
+  <meta content="Платний хостинг Україна - хостинг з php mysql, безкоштовний хостинг" name=description>
+  <meta content="платний хостинг україна дешевий php mysql безкоштовний безплатний ua" name=keywords>
+  <meta http-equiv=content-type content="text/html; charset=windows-1251">
+  <style type="text/css">
+<!--
+.style1 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style10 {font-size: 10px}
+-->
+  </style>
+</HEAD>
+<BODY>
+<div align="center" class="style1">
+  <table width="100%"  border="0">
+    <tr>
+      <td><p align="center" class="style1">Ваш <a href="http://uahosting.com.ua" target="_blank">хостинг-акаунт</a> <br>
+        в компанії <a href="http://uahosting.com.ua" target="_blank">uahosting.com.ua</a> <br>
+  успішно створений.</p>
+        <table width="80%"  border="0" align="center">
+          <tr class="style10">
+            <td width="50%"><span class="style10">Далі Вам необхідно: </span></td>
+            <td width="50%" class="style10"> </td>
+          </tr>
+          <tr class="style10">
+            <td width="50%"><span class="style10">1. Зайти у Файл Менеджер в Панелі Керування Сайтом </span></td>
+            <td class="style10">(http://www.Ваш.Домен:2082)</td>
+          </tr>
+          <tr class="style10">
+            <td width="50%"><span class="style10">2. Видалити файл index.php у папці "www"</span></td>
+            <td class="style10">(Клікнути по назві файлу та у правому фреймі вибрати "Delete File")</td>
+          </tr>
+          <tr class="style10">
+            <td width="50%"><span class="style10">3. Завантажити файли Вашого сайту у папку "www"</span></td>
+            <td class="style10">(Можна використовувати ФТП-доступ для керування файлами сайту)</td>
+          </tr>
+          <tr class="style10">
+            <td width="50%" valign="top"><span class="style10">4. Технічна допомога: </span></td>
+            <td class="style10"><p>Форум:<br>
+                    <a href="http://uahosting.com.ua/forum/" target="_blank">http://uahosting.com.ua/forum/</a> </p>
+                <p>Сторінка контактів:<br>
+                    <a href="http://uahosting.com.ua/contacts.php" target="_blank">http://uahosting.com.ua/contacts.php</a> </p>
+                <p>  </p></td>
+          </tr>
+        </table>
+        <p align="center" class="style1">Дякуємо за вибір компанії <a href="http://uahosting.com.ua" target="_blank">uahosting.com.ua</a> <br>
+  в якості Вашого <a href="http://uahosting.com.ua" target="_blank">хостинг-провайдера</a>.</p></td>
+      <td><p align="center" class="style1">Ваш <a href="http://uahosting.com.ua" target="_blank">хостинг-аккаунт</a> <br>
+        в компании <a href="http://uahosting.com.ua/ru/" target="_blank">uahosting.com.ua</a> <br>
+  успешно создан.</p>
+        <table width="80%"  border="0" align="center">
+          <tr>
+            <td width="50%"><span class="style10">Дальше Вам необходимо: </span></td>
+            <td width="50%" class="style10"> </td>
+          </tr>
+          <tr>
+            <td width="50%"><span class="style10">1. Зайти в Файл Менеджер в Панели Управления Сайтом </span></td>
+            <td class="style10">(http://www.Ваш.Домен:2082)</td>
+          </tr>
+          <tr>
+            <td width="50%"><span class="style10">2. Удалить файл index.php в папке "www"</span></td>
+            <td class="style10">(Кликнуть по названию файла и в правом фрейме выбрать "Delete File")</td>
+          </tr>
+          <tr>
+            <td width="50%"><span class="style10">3. Загрузить файлы Вашего сайта в  папку "www"</span></td>
+            <td class="style10">(Можна использовать ФТП-доступ для управления файлами сайта)</td>
+          </tr>
+          <tr>
+            <td width="50%" valign="top"><span class="style10">4. Техническая помощь: </span></td>
+            <td class="style10"><p>Форум:<br>
+                    <a href="http://uahosting.com.ua/forum/" target="_blank">http://uahosting.com.ua/forum/</a> </p>
+                <p>Страница контактов:<br>
+                    <a href="http://uahosting.com.ua/contacts.php" target="_blank">http://uahosting.com.ua/contacts.php</a> </p>
+                <p>  </p></td>
+          </tr>
+        </table>
+        <p align="center" class="style1">Благодарим за выбор компании <a href="http://uahosting.com.ua" target="_blank">uahosting.com.ua</a> <br>
+  в качестве Вашего <a href="http://uahosting.com.ua" target="_blank">хостинг-провайдера</a>.</p></td>
+    </tr>
+  </table>
+</div>
+</BODY>
+</HTML>
